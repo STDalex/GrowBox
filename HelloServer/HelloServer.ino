140f46db32ed3c0b658bd5f1f119a29db2ca8189
STDalex <stadnitskya@gmail.com>
2018-10-08 15:14:14 +0300

    add many functionality

--- HelloServer/HelloServer.ino
+++ HelloServer/HelloServer.ino
@@ -8,7 +8,11 @@
 #include <sstream>
 #include <limits>
 #include <iomanip>
-#include "DHT.h"
+#include "DHTesp.h"
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <Preferences.h>
+
 using namespace std;
 
 
@@ -21,7 +25,11 @@ uint8_t temprature_sens_read();
 #endif
 uint8_t temprature_sens_read();
 
-DHT dht(13, DHT22);
+#define led 25
+
+DHTesp dht;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, 10800);
 
 //const char* ssid = "AndroidAPA5A0";
 //const char* password = "hbmt2459";
@@ -32,10 +40,32 @@ U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/
 
 WiFiServer server(80);
 
-const int led = 25;
+
 String LED_State = "off";
 
-String header;
+int m_phase; // growth stage 
+int m_phase_state [4];
+bool m_is_new_day = true;
+int m_start_growth; // 0 - stop growth, 1 - start growth
+
+String header; // header for contain params for web client
+
+
+Preferences preferences;
+
+hw_timer_t * timer = NULL;
+volatile SemaphoreHandle_t timerSemaphore;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+//int interruptCounter = 0;
+void IRAM_ATTR onTimer() {
+//  portENTER_CRITICAL_ISR(&timerMux);
+ // interruptCounter++;
+  timeClient.update();
+//  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+//  Serial.println(interruptCounter);
+//  portEXIT_CRITICAL_ISR(&timerMux);
+}
 
 void setup(void) {
   pinMode(led, OUTPUT);
@@ -45,8 +75,16 @@ void setup(void) {
   u8x8.begin();
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8.drawString(0, 0, "Hello!");
-  
-  dht.begin();
+
+  dht.setup(13, DHTesp::DHT22);
+  timeClient.begin();
+  preferences.begin("m_phase-save", false);
+  m_phase = preferences.getInt("m_phase", 0);
+  m_phase_state[0] = preferences.getInt("m_phase_state_0", 0);
+  m_phase_state[1] = preferences.getInt("m_phase_state_1", 0);
+  m_phase_state[2] = preferences.getInt("m_phase_state_2", 0);
+  m_phase_state[3] = preferences.getInt("m_phase_state_3", 0);
+  m_start_growth = preferences.getInt("m_start_growth", 0);
   
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -67,41 +105,40 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  // Create semaphore to inform us when the timer has fired
+  timerSemaphore = xSemaphoreCreateBinary();
+  timer = timerBegin(3, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 5000000, true);
+ // timerAlarmEnable(timer);
 }
 
 void loop(void) {
+  
+  float uC_temp = (temprature_sens_read() - 32) / 1.8;
+  float dht22_temp = dht.getTemperature();
+  float dht22_hum = dht.getHumidity();
+  String s_dht22_temp = FloatToString(dht22_temp,2).c_str();
+  String s_dht22_hum = FloatToString(dht22_hum,2).c_str();
 
-  float temp = (temprature_sens_read() - 32) / 1.8;
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  float hic = dht.computeHeatIndex(t, h, false);
-  
-  stringstream stream_internal_temp;
-  stringstream stream_dht22_t;
-  stringstream stream_dht22_h;
-  
-  stream_internal_temp << fixed << "int t = "<< setprecision(2) << temp << " C";
-  stream_dht22_t << fixed << "ext t = "<< setprecision(2) << hic << " C";
-  stream_dht22_h << fixed << "hum = "<< setprecision(2) << h << " %";
-  
-  string s_int_temp = stream_internal_temp.str();
-  string s_dht22_t = stream_dht22_t.str();
-  string s_dht22_h = stream_dht22_h.str();
-  
-  u8x8.drawString(0, 1, s_int_temp.c_str());
-  u8x8.drawString(0, 2, s_dht22_t.c_str());
-  u8x8.drawString(0, 3, s_dht22_h.c_str());
+  check_time();
+  String s_count_phase_0 = FloatToString(m_phase_state[0],0).c_str();
+  String s_count_phase_1 = FloatToString(m_phase_state[1],0).c_str();
+  String s_count_phase_2 = FloatToString(m_phase_state[2],0).c_str();
+  String s_count_phase_3 = FloatToString(m_phase_state[3],0).c_str();
 
+  update_oled(uC_temp, dht22_temp, dht22_hum);
 
   WiFiClient client = server.available();
-
+  
   if (client) {
     Serial.println("New Client.");
     String currentLine = "";
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
+     //   Serial.write(c);
         header += c;
         if (c == '\n') {
           if (currentLine.length() == 0) {
@@ -109,35 +146,109 @@ void loop(void) {
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            
-            if (header.indexOf("GET /LED/on") >= 0) {
-              LED_State = "on";
-              digitalWrite(led, HIGH);
-            } else if (header.indexOf("GET /LED/off") >= 0) {
-              LED_State = "off";
-              digitalWrite(led, LOW);
-            } 
+
+            if (header.indexOf("GET /action_set_m_phase?m_phase=0") >= 0 ) {
+              m_phase = 0;
+              preferences.putInt("m_phase", m_phase);
+            }
+            else if (header.indexOf("GET /action_set_m_phase?m_phase=1") >= 0 ) {
+              m_phase = 1;
+              preferences.putInt("m_phase", m_phase);
+            }            
+            else if (header.indexOf("GET /action_set_m_phase?m_phase=2") >= 0 ) {
+              m_phase = 2;
+              preferences.putInt("m_phase", m_phase); 
+            }
+            else if (header.indexOf("GET /action_set_m_phase?m_phase=3") >= 0 ) {
+              m_phase = 3;
+              preferences.putInt("m_phase", m_phase); 
+            }
+            if (header.indexOf("GET /new?psw=121") >= 0 ) {
+              if (m_start_growth == 0){
+                m_start_growth = 1;
+                preferences.putInt("m_start_growth", 1);
+              }
+              else {
+                m_start_growth = 0;
+                preferences.putInt("m_start_growth", 0);
+                for (int i =0; i<sizeof(m_phase_state); i++)
+                  m_phase_state[i] = 0;
+                preferences.putInt("m_phase_state_0", 0);
+                preferences.putInt("m_phase_state_1", 0);
+                preferences.putInt("m_phase_state_2", 0);
+                preferences.putInt("m_phase_state_3", 0);
+                m_phase = 0;
+                preferences.putInt("m_phase", m_phase);
+                setLED("off");
+                break;
+              }
+            }
             
             
             client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <meta http-equiv=\"refresh\" content=\"30\">");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <meta http-equiv=\"refresh\" content=\"20; url=/\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
             
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white;");
+            client.println("text-decoration: none; margin: 2px; padding: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #555555;}</style></head>");
             
-            client.println("<body><h1>GROW BOX</h1>");
+            client.println("<body><h1><a href=\"/\">GROWTH BOX</a></h1>");
             
             
-            client.println("<p>LED State " + LED_State + "</p>");
-            if (LED_State=="off") {
+            
+         /*   if (LED_State=="off") {
               client.println("<p><a href=\"/LED/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/LED/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
+            }*/
+            client.println("<form action=\"/new\">\n");
+            if (m_start_growth == 0)            
+              client.println("<input type=\"submit\" class=\"button\" value=\"START GROWTH\">\n");
+            else
+              client.println("<input type=\"submit\" class=\"button button2\" value=\"STOP GROWTH\">\n");
+            client.println("<input type=\"password\" name=\"psw\">\n");
+            client.println("</form>");
+            
+            client.println("<h3> Select growth stage</h3>");
+            client.println("<form action=\"/action_set_m_phase\">");
+            if (m_phase==0) {
+            client.println("<input type=\"radio\" name=\"m_phase\" value=0 checked> Germination<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=1> Seedling<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=2> Vegetative<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=3> Flowering<br>");
+            }
+            if (m_phase==1) {
+            client.println("<input type=\"radio\" name=\"m_phase\" value=0> Germination<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=1 checked> Seedling<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=2> Vegetative<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=3> Flowering<br>");
+            }
+            if (m_phase==2) {
+            client.println("<input type=\"radio\" name=\"m_phase\" value=0> Germination<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=1> Seedling<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=2 checked> Vegetative<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=3> Flowering<br>");
+            }
+            if (m_phase==3) {
+            client.println("<input type=\"radio\" name=\"m_phase\" value=0> Germination<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=1> Seedling<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=2> Vegetative<br>");
+            client.println("<input type=\"radio\" name=\"m_phase\" value=3 checked> Flowering<br>");
+            }    
+            client.println("<input type=\"submit\" value=\"Submit\">");
+            client.println("</form><br>");
+
+            client.println("<h3> Curent state</h3>");
+            client.println("<p>LED State " + LED_State + "</p>");
+            client.println("<p>Germination " + s_count_phase_0 + " days</p>");
+            client.println("<p>Seedling " + s_count_phase_1 + " days</p>");
+            client.println("<p>Vegetative " + s_count_phase_2 + " days</p>");
+            client.println("<p>Flowering " + s_count_phase_3 + " days</p>");
+            client.println("<p>Temperature is " + s_dht22_temp + " C</p>");
+            client.println("<p>Humidity is " + s_dht22_hum + " %</p>");
+
             client.println("</body></html>");
             
             client.println();
@@ -155,4 +266,66 @@ void loop(void) {
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+}
+
+void update_oled(float uC_temp, float dht_temp, float dht_hyg) {
+  u8x8.drawString(0, 1, ("uC t = " + FloatToString(uC_temp, 2) + " C").c_str());
+  u8x8.drawString(0, 2, ("ext t = " + FloatToString(dht_temp, 2) + " C").c_str());
+  u8x8.drawString(0, 3, ("hum = " + FloatToString(dht_hyg, 2) + " %").c_str());
+}
+
+void check_time(){
+   timeClient.update();
+   unsigned long ep = timeClient.getEpochTime();
+   int s = timeClient.getSeconds();
+   if (m_start_growth == 1) {
+   switch (m_phase){
+    case 0 : {s < 0 ? setLED("on") : setLED("off"); break;}
+    case 1 : {s < 20 ? setLED("on") : setLED("off"); break;}
+    case 2 : {s < 45 ? setLED("on") : setLED("off"); break;}
+    case 3 : {s < 30 ? setLED("on") : setLED("off"); break;}
+    default : {s < 45 ? setLED("on") : setLED("off"); Serial.println("strange phase detected on check_time()"); break;}
+   }
+   if (s == 0 && m_is_new_day) { 
+    m_phase_state[m_phase]++;
+    m_is_new_day = false;
+    switch (m_phase) {
+      case 0 : {preferences.putInt("m_phase_state_0", m_phase_state[m_phase]); break;}
+      case 1 : {preferences.putInt("m_phase_state_1", m_phase_state[m_phase]); break;}
+      case 2 : {preferences.putInt("m_phase_state_2", m_phase_state[m_phase]); break;}
+      case 3 : {preferences.putInt("m_phase_state_3", m_phase_state[m_phase]); break;}
+      default : {Serial.println("strange phase detected on check_time()"); break;}
+    }
+   }
+   if (s == 1)
+    m_is_new_day = true;
+   }
+   Serial.print("m_phase is ");
+   Serial.println(m_phase);
+   Serial.print("Second is ");
+   Serial.println(s);
+   Serial.print("epohal is ");
+   Serial.println(ep);
+}
+
+void setLED(string state) {
+  if (state == "on") {
+    LED_State = "on";
+    digitalWrite(led, HIGH); 
+    Serial.print("LED set to ");
+    Serial.println(state.c_str());
+  }
+  if (state == "off") {
+    LED_State = "off";
+    digitalWrite(led, LOW);
+    Serial.print("LED set to ");
+    Serial.println(state.c_str());
+  }
+}
+
+string FloatToString(float value, uint precision) {
+  stringstream stream;
+  stream << fixed << setprecision(precision) << value;
+  string s_value = stream.str();
+  return s_value;
 }
